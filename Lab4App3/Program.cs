c864a9bc5d3fea113008cf1e212e69e6f9669ad9
Simplex Constr <Simplex Constr@NEWTON01>
2021-10-07 14:15:00 +0300

    Добавьте файлы проекта.

---
+++ Lab4App3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ka = 0; //квадратов по А
+            int kb = 0; //квадратов по Б
+            int k = 0; // Общее кол-во квадратов
+            Console.Write("Введите число А> 0:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число Б> 0:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число С> 0:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            while (c <= a)
+            {
+                ka++;
+                a = a - c;
+            }
+            while (c <= b)
+            {
+                kb++;
+                b = b - c;
+
+            }
+            for (int i = 0; i < kb; i++)
+            {
+                k = k + ka;
+
+            }
+            Console.WriteLine($"Число квадратов:{k}");
+            Console.ReadKey();
+        }
+    }
+}
